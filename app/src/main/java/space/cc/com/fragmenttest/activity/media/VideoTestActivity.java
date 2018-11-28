26fa0aa4ce075b92d0bc3d14941fd5f7e4f1cbf1
cc-ohayou <qusiba.0>
2018-11-29 00:09:31 +0800

    videoView简单实验 引入开源的饺子播放器组件为下一步测试视频组件做准备

--- app/src/main/java/space/cc/com/fragmenttest/activity/media/VideoTestActivity.java
+++ app/src/main/java/space/cc/com/fragmenttest/activity/media/VideoTestActivity.java
@@ -10,6 +10,7 @@ import android.support.annotation.NonNull;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.VideoView;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -22,12 +23,14 @@ import space.cc.com.fragmenttest.domain.Ring;
 public class VideoTestActivity extends BaseActivity implements View.OnClickListener,View.OnTouchListener {
 
     private MediaPlayer mediaPlayer = new MediaPlayer();
+    private VideoView videoView ;
     private ImageButton play;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_test);
         play=findViewById(R.id.play);
+        videoView=findViewById(R.id.videoView);
         setImageButOnclickListenerByRid(R.id.play, this);
         setImageButOnclickListenerByRid(R.id.stop, this);
         setImageButOnclickListenerByRid(R.id.pause, this);
@@ -38,10 +41,26 @@ public class VideoTestActivity extends BaseActivity implements View.OnClickListe
             requirePermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
         } else {
             //初始化播放器
-            initMediaPlayer();
+//            initMediaPlayer();
+            initVideoView();
         }
     }
-
+  /**
+     * @description 初始化mediaPlayer 播放音频
+   * 常用方法api
+   * setDataSource 设置要播放的音频文件位置
+   * prepare 再开始播放前完成准备工作 不过一般都调用 asyncPrepare
+   * start
+   * pause
+   * reset
+   * seekTo 从指定位置开始播放
+   * stop
+   * release
+   * isPlaying
+   * getDuration 获取载入的音频文件时长
+     * @author CF
+     * created at 2018/11/28/028  23:11
+     */
     private void initMediaPlayer() {
         try {
             File file = new File("/external/audio/media","1332.mp3");
@@ -58,6 +77,25 @@ public class VideoTestActivity extends BaseActivity implements View.OnClickListe
         }
     }
 
+  /**
+     * @description 初始化视频播放器
+   *  常用方法
+   *  setVideoPath
+   *  start
+   *  pause
+   *  resume 重头播放
+   *  seekTo 指定位置开始播放
+   *  isPlaying
+   *  getDuration 获取载入的视频文件的时长
+   *
+     * @author CF
+     * created at 2018/11/28/028  23:15
+     */
+    void  initVideoView(){
+        toastSimple(Environment.getExternalStorageDirectory().getPath());
+        File file=new File(Environment.getExternalStorageDirectory(),"qsj.mp4");
+        videoView.setVideoPath(file.getPath());
+    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -65,7 +103,8 @@ public class VideoTestActivity extends BaseActivity implements View.OnClickListe
         switch(requestCode){
             case 1:
                 if(grantSucc(grantResults)){
-                    initMediaPlayer();
+                    initVideoView();
+//                    initMediaPlayer();
                 }else{
                     toastSimple("拒绝权限将无法使用该程序");
                     finish();
@@ -81,24 +120,33 @@ public class VideoTestActivity extends BaseActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.play:
-                if (!mediaPlayer.isPlaying()) {
+                if(!videoView.isPlaying()){
+                    videoView.start();
+                }
+                /*if (!mediaPlayer.isPlaying()) {
                     mediaPlayer.start();
                     play.setImageResource(R.drawable.pause);
                     toastSimple("start play mediaPlayer.isPlaying()="+mediaPlayer.isPlaying());
-                }
+                }*/
                 break;
             case R.id.pause:
-                if (mediaPlayer.isPlaying()) {
+                if(videoView.isPlaying()){
+                    videoView.pause();
+                }
+               /* if (mediaPlayer.isPlaying()) {
                     mediaPlayer.pause();
                     play.setImageResource(R.drawable.play);
                     toastSimple("pause play now");
 
                 }else{
                     toastSimple("not playing now");
-                }
+                }*/
                 break;
             case R.id.stop:
-                if (mediaPlayer.isPlaying()) {
+                if(videoView.isPlaying()){
+                    videoView.resume();
+                }
+                /*if (mediaPlayer.isPlaying()) {
                     //停止播放
                     mediaPlayer.reset();
                     initMediaPlayer();
@@ -106,7 +154,7 @@ public class VideoTestActivity extends BaseActivity implements View.OnClickListe
                     toastSimple("stop play now");
                 }else{
                     toastSimple("not playing now");
-                }
+                }*/
                 break;
             case R.id.showDir:
                 RingtoneManager ringtoneManager = new RingtoneManager(this); // 铃声管理器
@@ -133,6 +181,10 @@ public class VideoTestActivity extends BaseActivity implements View.OnClickListe
             mediaPlayer.release();
 
         }
+        if(videoView!=null){
+            videoView.suspend();
+        }
+
     }
 
     @Override
