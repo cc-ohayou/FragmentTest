61eda0ffb33cdac3b9a61114b868540870b27c93
caifei <caifei@ddy98.com>
2019-01-21 19:31:39 +0800

    解决toolbar伸缩折叠布局失败的问题 原因在于androidx.coordinatorlayout.widget.CoordinatorLayout用错了类 androidx.constraintlayout.widget.ConstraintLayout 浪费了很多时间 引以为戒  toolbar标题栏返回按钮替换

--- app/src/main/java/space/cc/com/fragmenttest/activity/OperBizDetailActivity.java
+++ app/src/main/java/space/cc/com/fragmenttest/activity/OperBizDetailActivity.java
@@ -1,8 +1,12 @@
 package space.cc.com.fragmenttest.activity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,6 +15,9 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import space.cc.com.fragmenttest.R;
 import space.cc.com.fragmenttest.domain.bizobject.IntentExtraKey;
 import space.cc.com.fragmenttest.domain.bizobject.OperateBiz;
@@ -18,45 +25,79 @@ import space.cc.com.fragmenttest.util.UtilBox;
 
 public class OperBizDetailActivity extends BaseActivity {
     private TextView operBizDesc;
+    private TextView operBizCreateTime;
+    private TextView operBizUpdateTime;
+
+    @BindView(R.id.operBiz_url)
+    TextView operBiz_url;
+    @BindView(R.id.operBiz_env)
+    TextView operBiz_env;
+    @BindView(R.id.operBiz_pro)
+    TextView operBiz_pro;
+
     private FloatingActionButton floatingActionButton;
+
     private CollapsingToolbarLayout collapsingToolbarLayout;
     private Toolbar oprBizToolBar;
     private ImageView imageView;
-    private ActionBar actionBar;
 
+    @SuppressLint("RestrictedApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_oper_biz_detail);
-        Intent intent=getIntent();
-        OperateBiz operateBiz= (OperateBiz) intent.getSerializableExtra(IntentExtraKey.OPER_BIZ_DETAIL.getValue());
+        ButterKnife.bind(this);
+        Intent intent = getIntent();
+        OperateBiz operateBiz = (OperateBiz) intent.getSerializableExtra(IntentExtraKey.OPER_BIZ_DETAIL.getValue());
         initView();
-        actionBar=getSupportActionBar();
-        if(actionBar!=null){
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
-        collapsingToolbarLayout.setTitle(operateBiz.getOperName());
-        UtilBox.box().picasso.loadDrawResIntoView(imageView,R.drawable.default_head);
-        operBizDesc.setText(generateOperBizDesc(operateBiz.getDesc()));
+        initToolBar();
+        initCollapseToolBar(operateBiz);
+        UtilBox.box().picasso.loadDrawResIntoView(imageView, R.drawable.default_head);
+        operBizDesc.setText("描述："+operateBiz.getDesc());
+        operBiz_url.setText("Url："+operateBiz.getUrl());
+        operBiz_pro.setText("项目："+operateBiz.getProject());
+        operBiz_env.setText("环境："+operateBiz.getEnvType());
+        operBizCreateTime.setText("创建时间："+operateBiz.getCreateTime());
+        operBizUpdateTime.setText("更新时间："+operateBiz.getUpdateTime());
     }
-private String generateOperBizDesc(String desc){
-        StringBuilder sb=new StringBuilder(desc);
-        for (int i=0;i<500;i++){
-            sb.append(desc);
-        }
-        return sb.toString();
-}
+
+    private void initCollapseToolBar(OperateBiz operateBiz) {
+        collapsingToolbarLayout.setTitle(operateBiz.getOperName());
+        collapsingToolbarLayout.setExpandedTitleTextColor(ColorStateList.valueOf(
+                ContextCompat.getColor(getBaseContext(),R.color.colorAccent)));
+        collapsingToolbarLayout.setCollapsedTitleTextColor(ColorStateList.valueOf(
+                ContextCompat.getColor(getBaseContext(),R.color.white)));
+    }
+
+    private void initToolBar() {
+        //设置对应的图片
+        oprBizToolBar.setNavigationIcon(R.drawable.common_title_back);
+        //处理点击
+        oprBizToolBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+
+
+
     private void initView() {
-        operBizDesc=findViewById(R.id.operBiz_desc);
-        collapsingToolbarLayout=findViewById(R.id.operBiz_collapingBar);
-        oprBizToolBar=findViewById(R.id.operBiz_toolbar);
-        imageView=findViewById(R.id.operBiz_image_view);
+        operBizDesc = findViewById(R.id.operBiz_desc);
+        operBizCreateTime = findViewById(R.id.operBiz_createTime);
+        operBizUpdateTime = findViewById(R.id.operBiz_updateTime);
+
+        collapsingToolbarLayout = findViewById(R.id.operBiz_collapingBar);
+        oprBizToolBar = findViewById(R.id.operBiz_toolbar);
+        imageView = findViewById(R.id.operBiz_image_view);
     }
 
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()){
+        switch (item.getItemId()) {
             case android.R.id.home:
                 finish();
                 return true;
