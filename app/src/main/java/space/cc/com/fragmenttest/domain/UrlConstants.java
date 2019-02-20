6278e3a46dccedebf6ba5236fce7b8f59ea64668
caifei <caifei@ddy98.com>
2019-02-20 18:19:41 +0800

    url更改

--- app/src/main/java/space/cc/com/fragmenttest/domain/UrlConstants.java
+++ app/src/main/java/space/cc/com/fragmenttest/domain/UrlConstants.java
@@ -1,8 +1,8 @@
 package space.cc.com.fragmenttest.domain;
 
 public class UrlConstants {
-    public static  String PREFIX="http://118.31.73.19:8002/";
-    public static final String PREFIX02="http://118.31.73.19:3004/";
+    public static  String PREFIX="http://ccspace.info/api/ccspace/";
+    public static final String PREFIX02="http://ccspace.info:3004/";
     public static final String PREFIX03="http://txt.janpn.com/to/txt/148/148372.txt";
     //下载源要选择好 否则DownLoadTask的Progress获取到的status一直是waiting状态 不是loading状态
     // 此时进度刷新的回调方法onProgress不会被执行也就不会更新下载进度
